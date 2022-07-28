bf77526b0460ba8a59be948f2ce112386df0a73a
Neal Stublen <neal@youneedabudget.com>
2022-07-28 09:55:17 -0400

    Strip down project to an empty recycler view

--- app/src/main/java/com/example/jumpingrv/RecyclerFragment.kt
+++ app/src/main/java/com/example/jumpingrv/RecyclerFragment.kt
@@ -5,15 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.jumpingrv.databinding.FragmentFirstBinding
+import com.example.jumpingrv.databinding.FragmentRecyclerBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class RecyclerFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentRecyclerBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,9 +21,9 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentRecyclerBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +31,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.recycler.apply {
         }
     }
 
